4d5f8016327c8ceb3566d2964258f650108f976e
kirrok <kirrok@mail.ru>
2016-05-01 18:33:59 +0300

    server
    listen and print

--- sync_server/main.cpp
+++ sync_server/main.cpp
@@ -90,7 +90,7 @@ int main() {
     socklen_t clilent_addr_size;
 
     size_t *header = new size_t;
-    char *buffer;
+    char *buffer = new char[256];
 
     sockaddr_in serv_addr, cli_addr;
 
@@ -122,11 +122,14 @@ int main() {
         if (newsockfd < 0)
             error("ERROR on accept");
 
-//        bzero(buffer, 256);
+        bzero(buffer, 256);
 
+//      n = read(newsockfd, header, sizeof(size_t));
         n = read(newsockfd, header, sizeof(size_t));
-	cout << (size_t)(*header) <<endl;
-// надо парсить размер и тип запроса
+
+	
+	//cout << (size_t)(*header) <<endl;
+	cout << buffer << endl;
         // n = write(newsockfd,json_object_to_json_string(jobj1),256);
 
 
